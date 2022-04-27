cf1d5ccfb1d2f2739617437803503f6006d8dfd5
Elocinacademia <nicolezhanacademia@gmail.com>
2022-04-27 23:34:17 +0100

    update

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ batch_size = 64
 
 # Create data loaders.
 train_dataloader = DataLoader(training_data, batch_size=batch_size)
+
 test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
 for X, y in test_dataloader:
