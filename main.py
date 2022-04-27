228b7986940af2a7cee3f4c9435d433d837d5bb1
Elocinacademia <nicolezhanacademia@gmail.com>
2022-04-27 10:27:08 +0100

    update

--- main.py
+++ main.py
@@ -11,27 +11,30 @@ from torchvision.transforms import ToTensor
 #`torchvision.datasets`module contains `Dataset` objects of real world dataset.
 #Every `Dataset` includes two arguments `transform` and `target_transform` to modify the samples and labels.
 
+# Download training data from open datasets.
 training_data = datasets.FashionMNIST(
-	root = "data",
-	train = True,
-	download = True,
-	transform = ToTensor(),
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor(),
 )
 
+# Download test data from open datasets.
 test_data = datasets.FashionMNIST(
-	root = "data",
-        train = False,,
-        download = True,
-        transform = ToTensor(),
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
 )
+
 
 batch_size = 64
 
-#Create data loaders
-training_dataloader = Dataloader(training_data, batch_size = batch_size)
-test_dataloader = Dataloader(test_data, batch_size = batch_size)
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
-for X,y in test_dataloader:
-	print(f"Shape of X [N,C,H,W]: {X.shape}")
-	print(f"Shape of y: {y.shape} {y.dtype}")
-	break
+for X, y in test_dataloader:
+    print(f"Shape of X [N, C, H, W]: {X.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    break
